2c4c2eff261112b2dd8a8e931a448f7e1fecf865
sajad-ahmed-soomro <sajadahmedsoomro321@gmail.com>
2025-07-07 17:53:20 +0500

    final push to github assignment01

--- assignment01/app/layout.js
+++ assignment01/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "Quote Generator",
-  description: "Next.js App Quote Generator",
+  description: "Next.js App Quote Generator Assignment01 Nexium",
 };
 
 export default function RootLayout({ children }) {
