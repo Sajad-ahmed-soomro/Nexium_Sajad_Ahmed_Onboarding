6830589d70613e4a8c1b8cad78cc1a6f2604c7a8
sajad-ahmed-soomro <sajadahmedsoomro321@gmail.com>
2025-07-16 23:12:44 +0500

    minor change

--- grand-project/my-app/app/page.js
+++ grand-project/my-app/app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
       <>
-        <h1>Health Tracker</h1>
+        <h1 className="leading-8 font-bold">Health Tracker</h1>
+        
       </>
     );
 }
